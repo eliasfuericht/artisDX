bfb06edcc6e65d687788d9c935ee249e8057f44b
Elias Füricht <eliafuer@gmail.com>
2024-10-28 22:59:54 +0100

    wth was there gl_Position in there?

--- shaders/triangle.vert.hlsl
+++ shaders/triangle.vert.hlsl
@@ -5,7 +5,7 @@ cbuffer ubo : register(b0)
     row_major float4x4 ubo_viewMatrix : packoffset(c8);
 };
 
-static float4 gl_Position;
+static float4 position;
 static float3 outColor;
 static float3 inColor;
 static float3 inPos;
@@ -19,13 +19,13 @@ struct SPIRV_Cross_Input
 struct SPIRV_Cross_Output
 {
     float3 outColor : COLOR;
-    float4 gl_Position : SV_Position;
+    float4 position : SV_Position;
 };
 
 void vert_main()
 {
     outColor = inColor;
-    gl_Position = mul(float4(inPos, 1.0f), mul(ubo_modelMatrix, mul(ubo_viewMatrix, ubo_projectionMatrix)));
+    position = mul(float4(inPos, 1.0f), mul(ubo_modelMatrix, mul(ubo_viewMatrix, ubo_projectionMatrix)));
 }
 
 SPIRV_Cross_Output main(SPIRV_Cross_Input stage_input)
@@ -34,7 +34,7 @@ SPIRV_Cross_Output main(SPIRV_Cross_Input stage_input)
     inPos = stage_input.inPos;
     vert_main();
     SPIRV_Cross_Output stage_output;
-    stage_output.gl_Position = gl_Position;
+    stage_output.position = position;
     stage_output.outColor = outColor;
     return stage_output;
 }
